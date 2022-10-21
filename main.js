a958fdca0bb5cc9d47d2ad3d6485853a3f9b1045
jaimezzapata <zapataval2304@gmail.com>
2022-10-21 15:06:04 -0500

    Proyecto de Front Sencillo

---
+++ main.js
@@ -0,0 +1,18 @@
+const open_btn = document.querySelector('#open-btn');
+const close_btn = document.querySelector('#close-btn');
+const navs = document.querySelectorAll('.nav')
+
+console.log(navs)
+
+open_btn.addEventListener('click', ()=>{
+  navs.forEach((nav)=> {
+    nav.classList.add('visible')
+  })
+});
+
+close_btn.addEventListener('click', ()=>{
+  navs.forEach((nav)=> {
+    nav.classList.remove('visible')
+  })
+});
+
